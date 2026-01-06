cc4896c8f329edd4f4b3243be458ddba2bf7da8b
Shai Nosatzki <shainosatzki@gmail.com>
2026-01-06 23:17:15 +0200

    Initiliaze project scripts

---
+++ Reference to project - dont submit!!!!!!/solution/grad_cam_analysis.py
@@ -0,0 +1,127 @@
+"""Show network train graphs and analyze training results."""
+import os
+import argparse
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from torch.utils.data import DataLoader
+
+from common import FIGURES_DIR
+from utils import load_dataset, load_model
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+# Arguments
+def parse_args():
+    """Parse script arguments.
+
+    Returns:
+        Namespace with model name, checkpoint path and dataset name.
+    """
+    parser = argparse.ArgumentParser(description='Analyze network performance.')
+    parser.add_argument('--model', '-m',
+                        default='XceptionBased', type=str,
+                        help='Model name: SimpleNet or XceptionBased.')
+    parser.add_argument('--checkpoint_path', '-cpp',
+                        default='checkpoints/XceptionBased.pt', type=str,
+                        help='Path to model checkpoint.')
+    parser.add_argument('--dataset', '-d',
+                        default='fakes_dataset', type=str,
+                        help='Dataset: fakes_dataset or synthetic_dataset.')
+
+    return parser.parse_args()
+
+
+def get_grad_cam_visualization(test_dataset: torch.utils.data.Dataset,
+                               model: torch.nn.Module) -> tuple[np.ndarray,
+                                                                torch.tensor]:
+    """Return a tuple with the GradCAM visualization and true class label.
+
+    Args:
+        test_dataset: test dataset to choose a sample from.
+        model: the model we want to understand.
+
+    Returns:
+        (visualization, true_label): a tuple containing the visualization of
+        the conv3's response on one of the sample (256x256x3 np.ndarray) and
+        the true label of that sample (since it is an output of a DataLoader
+        of batch size 1, it's a tensor of shape (1,)).
+    """
+    """INSERT YOUR CODE HERE, overrun return."""
+    from pytorch_grad_cam import GradCAM
+    from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+    from pytorch_grad_cam.utils.image import show_cam_on_image
+
+    target_layers = [model.conv3]
+    input_tensor = DataLoader(test_dataset, batch_size=1, shuffle=True)  # Create an input tensor image for your model..
+    # Note: input_tensor can be a batch tensor with several images!
+    # Recovers the original `dataset` from the `dataloader`
+    dataset = input_tensor.dataset
+
+    # Get a random sample
+    sample = next(iter(input_tensor))
+
+    # Extract the image and true label from the sampled batch
+    image_sample, true_label = sample[0], sample[1]
+    cam = GradCAM(model=model, target_layers=target_layers)
+
+    # You can also use it within a with statement, to make sure it is freed,
+    # In case you need to re-create it inside an outer loop:
+    # with GradCAM(model=model, target_layers=target_layers) as cam:
+    #   ...
+
+    # We have to specify the target we want to generate
+    # the Class Activation Maps for.
+    # If targets is None, the highest scoring category
+    # will be used for every image in the batch.
+    # Here we use ClassifierOutputTarget, but you can define your own custom targets
+    # That are, for example, combinations of categories, or specific outputs in a non standard model.
+
+    # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+    targets = [ClassifierOutputTarget(true_label)]
+    grayscale_cam = cam(input_tensor=image_sample, targets=targets)
+    # In this example grayscale_cam has only one image in the batch:
+    grayscale_cam = grayscale_cam[0, :]
+    img_array = image_sample[0].numpy()
+
+    # Normalize the pixel values to the range [0, 1]
+    image_normalized = img_array / 255.0
+    visualization = show_cam_on_image(np.transpose(image_normalized, (1, 2, 0)), grayscale_cam, use_rgb=True)
+
+    # You can also get the model outputs without having to re-inference
+    model_outputs = cam.outputs
+    return visualization, true_label
+
+
+def main():
+    """Create two GradCAM images, one of a real image and one for a fake
+    image for the model and dataset it receives as script arguments."""
+    args = parse_args()
+    test_dataset = load_dataset(dataset_name=args.dataset, dataset_part='test')
+
+    model_name = args.model
+    model = load_model(model_name)
+    model.load_state_dict(torch.load(args.checkpoint_path)['model'], strict=False)
+
+    model.eval()
+    seen_labels = []
+    while len(set(seen_labels)) != 2:
+        visualization, true_label = get_grad_cam_visualization(test_dataset,
+                                                               model)
+        grad_cam_figure = plt.figure()
+        plt.imshow(visualization)
+        title = 'Fake Image' if true_label == 1 else 'Real Image'
+        plt.title(title)
+        seen_labels.append(true_label.item())
+        grad_cam_figure.savefig(
+            os.path.join(FIGURES_DIR,
+                         f'{args.dataset}_{args.model}_'
+                         f'{title.replace(" ", "_")}_grad_cam.png'))
+
+
+if __name__ == "__main__":
+    main()
